5ec9f53431375ecd11d13b64e076931906e3d6f7
Fran-Kee Brown <106333841+fbrowndev@users.noreply.github.com>
2025-03-27 23:22:19 -0400

    Input Actions Created
    -Rotation
    -Propulsion

---
+++ Source/UnrealArch/Private/Sub.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Sub.h"
+
+// Sets default values
+ASub::ASub()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASub::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Display, TEXT("The Impulse strength is: %f"), ImpulseStrength);
+	UE_LOG(LogTemp, Display, TEXT("The torque strength is: %f"), TorqueStrength);
+}
+
+// Called every frame
+void ASub::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASub::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
